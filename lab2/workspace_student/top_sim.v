2008b5257640570e4cb0833025095b39af6e3dcc
vielooo <arordavid@gmail.com>
2021-05-26 15:52:29 +0800

    add function test
    cordic function test

--- lab2/workspace_student/top_sim.v
+++ lab2/workspace_student/top_sim.v
@@ -13,16 +13,65 @@ top toptest(
     .rd(rd),
     .byte(bytes),
     .addr(addr),
-    .wdata(wdata)
+    .wdata(wdata),
     .rdata_v(rdata_v),
     .rdata(rdata)
 );
 always #5 clk = ~clk;
 initial begin
+// c
     // start rst 
-    #10 rst_n = 'b0;
+    /* test case cases
+    {SIN, 0x1000a, 0xfc1b},
+	{SIN, 0x10014, 0xf08c},
+	{COS, 0x1000a, 0xffffd38d},
+	{COS, 0x10014, 0xffffa872}
+    */
+    #20 rst_n = 'b1;  rd= 'b0; wr= 'b0; bytes= 'h0;addr= 'h0;wdata= 'h0;
+    #20 rst_n = 'b0;
     #20 rst_n = 'b1; 
-    // test one, 
-    #6 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h0;wdata= 'hab;
-    #50  rd= 'b1; wr= 'b0; bytes= 'hf;addr= 'hab;
+    // test one, wait 5 cycles 
+    //byte always = oxf
+    // while, test write twice:
+    //1. write phase , wait for 5 cycles
+    //2. write config, wait for5 cycles 
+    // write phrase to 1 
+    #16 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h1;wdata= 'h1000a;
+    #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+    //wait 5 cycles, write sin config to 0x0
+    #50 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h0;wdata= 'h00000001;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+    //wait 5 + 30 cycles , read data
+    #350  rd= 'b1; wr= 'b0; bytes= 'hf;addr= 'h2;wdata= 'h0;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+
+     //test 2 
+     #100 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h1;wdata= 'h10014;
+    #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+    //wait 5 cycles, write sin config to 0x0
+    #50 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h0;wdata= 'h00000001;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+    //wait 5 + 30 cycles , read data
+    #350  rd= 'b1; wr= 'b0; bytes= 'hf;addr= 'h2;wdata= 'h0;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+     //test 3
+      #100 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h1;wdata= 'h1000a;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+    //wait 5 cycles, write sin config to 0x0
+     #50 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h0;wdata= 'h00000101;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+    //wait 5 + 30 cycles , read data
+     #350  rd= 'b1; wr= 'b0; bytes= 'hf;addr= 'h2;wdata= 'h0;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+     //test 4
+      #100 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h1;wdata= 'h10014;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+    //wait 5 cycles, write sin config to 0x0
+     #50 rd= 'b0; wr= 'b1; bytes= 'hf;addr= 'h0;wdata= 'h00000101;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+    //wait 5 + 30 cycles , read data
+     #350  rd= 'b1; wr= 'b0; bytes= 'hf;addr= 'h2;wdata= 'h0;
+     #10  rd= 'b0; wr= 'b0; bytes= 'hf;addr= 'h0;wdata= 'h0;
+   
 end
+endmodule
