f91d37a39fef83af2fbdb04a0d3ba085cfa88f31
vielooo <arordavid@gmail.com>
2021-05-31 17:02:18 +0800

    lab3 finished
    finish lab3 code, pass all test in all_sim

--- lab3/workspace_student/blk_mem/sim/blk_mem.v
+++ lab3/workspace_student/blk_mem/sim/blk_mem.v
@@ -48,7 +48,7 @@
 
 
 // IP VLNV: xilinx.com:ip:blk_mem_gen:8.4
-// IP Revision: 2
+// IP Revision: 4
 
 `timescale 1ns/1ps
 
@@ -73,9 +73,9 @@ input wire [7 : 0] dina;
 (* X_INTERFACE_INFO = "xilinx.com:interface:bram:1.0 BRAM_PORTA DOUT" *)
 output wire [7 : 0] douta;
 
-  blk_mem_gen_v8_4_2 #(
-    .C_FAMILY("artix7"),
-    .C_XDEVICEFAMILY("artix7"),
+  blk_mem_gen_v8_4_4 #(
+    .C_FAMILY("kintex7"),
+    .C_XDEVICEFAMILY("kintex7"),
     .C_ELABORATION_DIR("./"),
     .C_INTERFACE_TYPE(0),
     .C_AXI_TYPE(1),
--- lab3/workspace_student/cache.v
+++ lab3/workspace_student/cache.v
@@ -11,11 +11,59 @@ module cache(
     input [31:0] rdata_from_mem,     // 内存读取的数据
     input rvalid_from_mem,          // 内存读取数据可用标志
     // 输出给CPU的信号
-    output [7:0] rdata_to_cpu,      // 输出给CPU的数据
-    output hit_to_cpu,              // 输出给CPU的命中标志
+    output reg [7:0] rdata_to_cpu,      // 输出给CPU的数据
+    output reg hit_to_cpu,              // 输出给CPU的命中标志
     // 输出给下层内存模块的信号
     output reg rreq_to_mem,         // 输出给下层内存模块的读请求
     output reg [12:0] raddr_to_mem  // 输出给下层模块的突发传输首地址
     );
-
+    reg [1:0] state ;
+    wire [36:0] douta;
+    //state machine ready => fetched <=> load 
+    always @(posedge clk or posedge reset) begin
+      if (reset)  state <= 'b0;
+      else if (rreq_from_cpu && state == 0) state <= 1;
+      else if (state == 1) begin 
+        if (douta[36]==1 && douta[35:32]== raddr_from_cpu[12:9]) state <= 0;
+        else state <= 2;
+      end 
+      else if (state == 2 && rvalid_from_mem) state <= 1; 
+    end
+    //data to cpu 
+    always@(raddr_from_cpu or  douta)begin
+      if (raddr_from_cpu[1:0]==0) rdata_to_cpu = douta[7:0];
+      else if (raddr_from_cpu[1:0]==1) rdata_to_cpu = douta[15:8];
+      else if (raddr_from_cpu[1:0]==2) rdata_to_cpu = douta[23:16];
+      else  rdata_to_cpu = douta[31:24];
+    end
+    // if hit 
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin 
+        hit_to_cpu <= 0;
+        end 
+        else if (state == 1)begin
+            if (douta[36]==1 && douta[35:32]== raddr_from_cpu[12:9]) hit_to_cpu <= 1;
+            else hit_to_cpu <= 0;
+        end 
+        else hit_to_cpu <= 'b0;
+    end
+    // if load from main memo
+     always @(posedge clk or posedge reset) begin
+      if (reset)  rreq_to_mem <= 'b0;
+      else if(state ==2) rreq_to_mem <= 'b1;
+      else rreq_to_mem <= 'b0;
+        
+    end
+    // head addr to main memo
+    always @(raddr_from_cpu)begin
+      raddr_to_mem = {raddr_from_cpu[12:2],2'b0};
+    end
+  cache_blk_mem lovelymem (
+    .clka(clk),    // input wire clka
+    .ena(1'b1),      // input wire ena
+    .wea(rvalid_from_mem),      // input wire [0 : 0] wea
+    .addra(raddr_from_cpu[8:2]),  // input wire [6 : 0] addra
+    .dina( {1'b1,raddr_from_cpu[12:9],rdata_from_mem[31:0]}),    // input wire [36 : 0] dina
+    .douta(douta)  // output wire [36 : 0] douta
+  );
 endmodule
